d1c06fafdc72afc34552d940667e6747b6c561c6
mikias7890 <mikitemesgen939@gmail.com>
2024-10-12 23:39:45 +0200

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 
 import "./App.css";
-import Routing from "./Pages/router";
+import Routing from "./Pages/Router";
 import { Datacontent } from "./Component/Dataprovider/Dataprovider";
 import { Type } from "./Utility/Action.type";
 import { auth } from "./Utility/firebase";
